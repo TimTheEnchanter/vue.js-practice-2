b2c5f209785e0f18b8f7f519f69fdc114ad18708
James Parrott <japarrott@shaw.ca>
2024-10-29 10:48:08 -0600

    attribute bindings

--- vue-practice-2/src/App.vue
+++ vue-practice-2/src/App.vue
@@ -6,6 +6,7 @@ const counter = reactive({
   count: 0
 })
 const message = ref('Hello World!')
+const titleClass = ref('title')
 
 console.log(message.value) // "Hello World!"
 console.log(counter.count) // 0
@@ -14,7 +15,14 @@ message.value = 'Changed'
 </script>
 
 <template>
+  <h1 :class="titleClass">Make me red</h1>
   <h1>{{ message.split('').reverse().join('') }}</h1>
   <h1>{{ message }}</h1>
 	<p>Count is: {{ counter.count }}</p>
 </template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
